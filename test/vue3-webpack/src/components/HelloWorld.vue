dfb3b79e742476b5f814c0df5c191c45c3b91621
幻月 <1712030209@qq.com>
2025-09-17 09:27:59 +0800

    fix: webpack import问题

---
+++ test/vue3-webpack/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <h2 @click="$emit('click')">
+  </h2>
+  <img
+    src="~assets/logo.png"
+    alt="HEROPY" />
+</template>
+
+<script>
+// "browser": "./dist/umd/index.min.js",
+import {$at} from 'i18n-auto-extractor'
+console.log($at,'$at')
+export default {
+  props: {
+    msg: { type: String }
+  },
+  emits: ['click'],
+  setup(props, { emit }) {
+    return {
+      $at:()=>{},
+      $emit: emit
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import "~/scss/main";
+
+  h2 {
+    font-size: 22px;
+    color: $color--primary;
+    & + img {
+      width: 120px;
+    }
+  }
+</style>
