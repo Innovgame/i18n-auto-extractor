b3d1913dc69091ff78e88581668cbd691937346a
幻月 <1712030209@qq.com>
2025-09-22 17:06:42 +0800

    fix: options替换问题

--- src/translater.js
+++ src/translater.js
@@ -13,14 +13,26 @@ function getFirstKey(obj) {
   }
 }
 
-export const $at = (zhText, options=undefined) => {
-    const langSet= globalThis.__CURRENT_LANG_SET__;
-    if(!langSet || langSet.lang==='zh-CN' || !langSet.langMap) return zhText;
-    const key = getFirstKey(langSet.langMap) || ''
-    const md5Str =key? md5(zhText).slice(0, key.length): '';
-    let text = langSet.langMap[md5Str] || zhText;
+export const $at11 = (zhText: string, options?: Record<string, any>) => {
+    const md5Str = CryptoJS.MD5(zhText).toString().slice(0, 10);
+    let text = lang.value === 'en' ? (enJson as any)[md5Str] || zhText : zhText;
     if (options) {
-        if(options.override && options.override[langSet.lang]){
+        Object.keys(options).forEach(key => {
+            text = text.replace(new RegExp(`\{\\s*${key}\\s*\}`, 'g'), options[key])
+        })
+    }
+    return text
+}
+export const $at = (zhText, options=undefined) => {
+    const langSet = globalThis.__CURRENT_LANG_SET__ || {};
+    let text = zhText;
+    if(langSet.langMap && langSet.lang && langSet.lang !=='zh-CN'){
+        const key = getFirstKey(langSet.langMap) || ''
+        const md5Str =key? md5(zhText).slice(0, key.length): '';
+        text = langSet.langMap[md5Str] || zhText;
+    }
+    if (options) {
+        if(options.override && langSet.lang && options.override[langSet.lang]){
             text = options.override[langSet.lang]
         }
         Object.keys(options).forEach(key => {
